4ef223eb29d7e5eba7efa8f9b1c5cd37dde32395
Jurgis Jaška <meklonas@yahoo.com>
2011-11-03 13:58:52 +0200

    Papildytas .gitignore, PHPUnit testų nustatymai.

---
+++ system/main.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Pagrindinis sistemos komponentas. Apdoroją komponentų ryšius ir programos vykdymo seką.
+ *
+ * PHP version 5
+ *
+ * Licensed under The MIT License. Redistributions of files must retain the above copyright notice.
+ * For the full copyright and license information, please view the LICENSE file that was distributed
+ * with this source code.
+ *
+ * @package    PackageName
+ * @author     Jurgis Jaška <meklonas@yahoo.com>
+ * @copyright  2011 Jurgis Jaška
+ * @license    http://codex.devyni.eu/license
+ * @link       http://codex.devyni.eu/package/PackageName
+ * @since      0.1.1-alpha
+ */
+
+class Main {
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * PHPUnit aplinkos duomenų tvarkimas.
+ *
+ * PHP version 5
+ *
+ * Licensed under The MIT License. Redistributions of files must retain the above copyright notice.
+ * For the full copyright and license information, please view the LICENSE file that was distributed
+ * with this source code.
+ *
+ * @package    tests
+ * @author     Jurgis Jaška <meklonas@yahoo.com>
+ * @copyright  2011 Jurgis Jaška
+ * @license    http://codex.devyni.eu/license
+ * @link       http://codex.devyni.eu/package/tests
+ * @since      0.1.1-alpha
+ */
+
